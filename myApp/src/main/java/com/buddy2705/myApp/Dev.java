33d08e19223ec988e3ae75ef6a8fcb25d0cd47bc
buddy2704 <Btetnowski@icloud.com>
2024-11-06 08:29:36 -0600

    Add files via upload

---
+++ myApp/src/main/java/com/buddy2705/myApp/Dev.java
@@ -0,0 +1,12 @@
+package com.buddy2705.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+
+    public void build(){
+        System.out.println("working");
+
+    }
+}
